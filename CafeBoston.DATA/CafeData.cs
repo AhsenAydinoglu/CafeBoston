9da160e587c82843fb9fd4b220e66c54ac44870a
AhsenAydinoglu <ahsenaydinoglu@gmail.com>
2022-10-18 15:10:45 +0300

    Data Layer

---
+++ CafeBoston.DATA/CafeData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    internal class CafeData
+    {
+        public int TableCount { get; set; } = 20;
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Order> ActiveOrders{ get; set; } = new List<Order>();
+        public List<Order> PastOrders { get; set; }= new List<Order> ();
+    }
+}
